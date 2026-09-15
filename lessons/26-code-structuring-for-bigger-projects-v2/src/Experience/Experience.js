38b4e556b7fade2dc4c06bddd0a60868ce672049
Anil Hitang <anilhitang@macmini.home>
2026-09-15 00:36:03 -0300

    Setup project structure

--- lessons/26-code-structuring-for-bigger-projects-v2/src/Experience/Experience.js
+++ lessons/26-code-structuring-for-bigger-projects-v2/src/Experience/Experience.js
@@ -2,6 +2,10 @@ import * as THREE from "three";
 import Sizes from "./Utils/Sizes.js";
 import Time from "./Utils/Time.js";
 import Camera from "./Camera.js";
+import Renderer from "./Renderer.js";
+import World from "./World/World";
+
+let instance = null;
 
 export default class Experience {
   constructor(canvas) {
@@ -12,6 +16,7 @@ export default class Experience {
       return instance;
     }
     instance = this;
+
     //Global access
     window.experience = this;
     this.canvas = canvas;
@@ -21,11 +26,13 @@ export default class Experience {
     this.time = new Time();
     this.scene = new THREE.Scene();
     this.camera = new Camera();
+    this.renderer = new Renderer();
+    this.world = new World();
 
     // Resize event
     this.sizes.on("resize", () => {
       // Listening for the resize event
-      console.log("A resize occurred");
+      // console.log("A resize occurred");
       //   console.log(this.sizes.width);
       //   console.log(this.sizes.height);
       //   console.log(this.sizes.pixelRatio);
@@ -39,6 +46,13 @@ export default class Experience {
     });
   }
 
-  resize() {}
-  update() {}
+  resize() {
+    this.camera.resize();
+    this.renderer.resize();
+  }
+
+  update() {
+    this.camera.update();
+    this.renderer.update();
+  }
 }
